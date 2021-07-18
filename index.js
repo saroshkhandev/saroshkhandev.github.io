3ee8ed109aed11e9b8a15fd73b9cabeb754a5269
Sarosh <saroshabdullah2013@gmail.com>
2021-07-19 02:14:51 +0530

    Added theme selector

---
+++ index.js
@@ -0,0 +1,25 @@
+// document.getElementById("theme-style")
+
+let themeDots = document.getElementsByClassName('theme-dot');
+
+for (var i = 0; i < themeDots.length; i++) {
+    themeDots[i].addEventListener('click', function () {
+        let mode = this.dataset.mode;
+        console.log("option clicked:", mode);
+        setTheme(mode);
+    });
+}
+
+function setTheme(mode) {
+    if (mode === "light") {
+      document.getElementById("theme-style").href = "default.css";
+    } else if (mode === "blue") {
+      document.getElementById("theme-style").href = "blue.css";
+    } else if (mode === "green") {
+      document.getElementById("theme-style").href = "green.css";
+    } else if (mode === "purple") {
+      document.getElementById("theme-style").href = "purple.css";
+    } else {
+        console.log("Error");
+    }
+} 
